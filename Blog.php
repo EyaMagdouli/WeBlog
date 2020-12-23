335a43732a68a98bd17232d22df42cc0635c2605
eya <eyamagdouli7@gmail.com>
2020-12-23 14:31:09 +0100

    first commit

---
+++ Blog.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap 4 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Blog.css">
+    <title>My Blog</title>
+</head>
+<body class="bg-light" >
+<div class="container-fluid">
+<nav class="navbar bg-secondary">
+   <a class="navbar-brand" href="#">MonBlog.com</a>
+</nav>
+        </nav>
+    <header>
+        <div class="jumbotron text-center">
+            <h1 class="display-5"><strong></strong>Bienvenue sur MonBlog.com</h1>
+        </div>
+    </header>
+</div>  
+
+
+
+<!-- Bootstrap with jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap Popper Js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- Bootstrap Js -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
+    <script src="Blog.js"></script>  
+</body>
+</html>
